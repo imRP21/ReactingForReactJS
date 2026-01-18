2bd7974d497be3904f1721d4e91709380d5acef2
Raj Patel <rajpa@microsoft.com>
2026-01-18 17:31:20 +0530

    version 1: adds UX aspects (suggested Abhay Doshi)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   const [x, setX] = useState('');
   const [y, setY] = useState('');
-  const [operation, setOperation] = useState('Add');
   const [result, setResult] = useState(null);
   
-  const handleCalculate = () => {
+  const handleCalculate = (op) => {
     const numX = parseInt(x);
     const numY = parseInt(y);
     if (isNaN(numX) || isNaN(numY)) {
       setResult('Please enter valid numbers.');
       return;
     }
-    if (operation === 'Add') {
-      const res = numX + numY;
-      setResult(res < 0 || res > 100 ? 'Result out of bounds [0...100]. Be mindful of the range.' : res);
-    } else {
-      const res = numX - numY;
-      setResult(res < 0 || res > 100 ? 'Result out of bounds [0...100]. Be mindful of the range.' : res);
-    }
+    const res = (op === '+' ? numX + numY : numX - numY);
+    setResult(res < 0 || res > 100 ? 'Result out of bounds [0...100]. Be mindful of the range.' : res);
   };
 
   return (
@@ -41,16 +35,11 @@ function App() {
           onChange={e => setY(e.target.value)}
           style={{ marginRight: '0.5rem', padding: '0.5rem' }}
         />
-        <select
-          value={operation}
-          onChange={e => setOperation(e.target.value)}
-          style={{ marginRight: '0.5rem', padding: '0.5rem' }}
-        >
-          <option value="Add">Add</option>
-          <option value="Subtract">Subtract</option>
-        </select>
-        <button onClick={handleCalculate} style={{ padding: '0.5rem 1rem' }}>
-          Calculate
+        <button className='calc-button' onClick={() => handleCalculate('+')}>
+          +
+        </button>
+        <button className='calc-button' onClick={() => handleCalculate('-')}>
+          -
         </button>
       </div>
       {result !== null && (
